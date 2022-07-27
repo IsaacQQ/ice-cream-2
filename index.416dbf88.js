af1cafa11b0ede0a8392c32bf4c1d862ceacff81
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-07-27 08:02:11 +0000

    Deploying to gh-pages from @ AlexAmanitas/iceCream-team-18@c06b2a401e652e6d85ddef684e82637690aea26d 🚀

---
+++ index.416dbf88.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){var e=window.pageYOffset||document.documentElement.scrollTop;document.querySelector(".header-scroll").style.background=0!==e?"rgba(255, 184, 202, 1)":"transparent"};
+//# sourceMappingURL=index.416dbf88.js.map
